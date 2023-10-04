ef9a26b13da7bdf7ebf5ea9824b07128c5069d37
adahmadali <ahmadali09787@gmail.com>
2023-10-04 15:23:33 +0600

    banner done without view and edit

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,23 +1,23 @@
 <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="owl-carousel header-carousel position-relative">
         <?php
-            require_once('functions/function.php');
+            require_once('config.php');
             $select = "SELECT * FROM `banners`";
             $query = mysqli_query($con,$select);
-            $data = mysqli_fetch_assoc($query);
+            
 
-        
+            while($data = mysqli_fetch_assoc($query)){
         
         ?>
-        <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
-            <img class="img-fluid" src="img/carousel-1.jpg" alt="">
+        <div class="owl-carousel-item position-relative" data-dot="<img src='Admin/uploads/<?= $data['banner_image']; ?>'>">
+            <img class="img-fluid" src="Admin/uploads/<?= $data['banner_image']; ?>" alt="">
             <div class="owl-carousel-inner">
                 <div class="container">
                     <div class="row justify-content-start">
                         <div class="col-10 col-lg-8">
-                            <h1 class="display-2 text-white animated slideInDown">Pioneers Of Solar And Renewable Energy</h1>
-                            <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                            <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                            <h1 class="display-2 text-white animated slideInDown"><?= $data['banner_title']; ?></h1>
+                            <p class="fs-5 fw-medium text-white mb-4 pb-3"><?= $data['banner_subtitle']; ?></p>
+                            <a href="<?= $data['banner_url']; ?>" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft"><?= $data['banner_button']; ?></a>
                         </div>
                     </div>
                 </div>
@@ -25,7 +25,7 @@
         </div>
 
         <?php
-        
+        }
         ?>
     </div>
 </div>
